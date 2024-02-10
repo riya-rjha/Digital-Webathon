6ba03fa31d46a82a5e760e2a4720f36ae73b9dd1
riya-rjha <riyaranjanjha2006@gmail.com>
2024-02-10 14:56:57 +0530

    Changes in Main page and created full time app

--- Application/Frontend/src/App.jsx
+++ Application/Frontend/src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Heading from './Components/Heading';
+import AddNotes from './Pages/AddNotes.jsx';
+import DeleteNotes from './Pages/DeleteNotes.jsx';
+import EditNotes from './Pages/EditNotes.jsx';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={<Heading />} />
+      <Route path='/notes/create' element={<AddNotes />} />
+      <Route path='/notes/delete/:id' element={<DeleteNotes />} />
+      <Route path='/notes/edit/:id' element={<EditNotes />} />
+    </Routes>
   )
 }
 
